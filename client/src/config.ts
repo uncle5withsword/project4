7499a79d0b0e0502e984281cdb2d2ea176d667e1
namln1 <uncle5withsword@gmail.com>
2024-05-12 16:45:55 +0700

    cmt

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = 'ub9kyve8h6';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-t5gjj1yfyomavcaa.us.auth0.com',
-  clientId: 'QKUKphh6YfPw2Oct3eYpBxbA007AbYRk',
-  callbackUrl: 'http://localhost:3000/callback'
+  domain: 'dev-2l8kjc4bw7bmg8aa.us.auth0.com',
+  clientId: 'uNaF8YfPnTPlVTB5U48IWu5SKiIauLoC',
+  callbackUrl: 'http://localhost:3000'
 };
