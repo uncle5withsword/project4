7af1563a0f411a9d4454facb45f198ab4e4194b7
namln1 <uncle5withsword@gmail.com>
2024-05-12 16:16:51 +0700

    cmt

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'ub9kyve8h6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-t5gjj1yfyomavcaa.us.auth0.com',
+  clientId: 'QKUKphh6YfPw2Oct3eYpBxbA007AbYRk',
+  callbackUrl: 'http://localhost:3000/callback'
+};
